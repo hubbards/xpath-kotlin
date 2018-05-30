26402f1a72094c11483b3fc32b7efa8879023f9b
Spencer Hubbard <hubbardspencerm@gmail.com>
2018-05-30 09:09:19 -0700

    Add function call expression

---
+++ src/main/kotlin/xpath/FunctionCall.kt
@@ -0,0 +1,38 @@
+package xpath
+
+/**
+ * A function call expression, see
+ * [specification](https://www.w3.org/TR/1999/REC-xpath-19991116/#section-Function-Calls).
+ *
+ * TODO add missing standard functions
+ */
+class FunctionCall(val name: String,
+                   vararg val arguments: Expression) : Expression() {
+  private fun helper(transform: (Expression) -> String) =
+      name + arguments.joinToString(
+          separator = ",",
+          prefix = "(",
+          postfix = ")",
+          transform = transform
+      )
+
+  override fun abbreviated() = helper(Expression::abbreviated)
+
+  override fun unabbreviated() = helper(Expression::unabbreviated)
+}
+
+/**
+ * Returns the number of nodes in [argument] node-set.
+ */
+fun count(argument: Expression) = FunctionCall("count", argument)
+
+/**
+ * Returns the local part of the expanded-name of the first node in [argument]
+ * node-set.
+ */
+fun localName(argument: Expression) = FunctionCall("local-name", argument)
+
+/**
+ * Returns the local part of the expanded-name of the context node.
+ */
+fun localName() = FunctionCall("local-name")
