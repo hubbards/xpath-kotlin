b7e3b7371aece352daec683ebf9bdbf3fa7f79a6
Spencer Hubbard <hubbardspencerm@gmail.com>
2018-06-14 19:18:20 -0700

    Refactor get operator for NodeList

--- src/main/kotlin/xpath/extension/NodeList.kt
+++ src/main/kotlin/xpath/extension/NodeList.kt
@@ -3,7 +3,7 @@ package xpath.extension
 import org.w3c.dom.Node
 import org.w3c.dom.NodeList
 
-operator fun NodeList.get(i: Int) = this.item(i)
+operator fun NodeList.get(i: Int) = item(i)
 
 operator fun NodeList.iterator() =
     object : Iterator<Node> {
