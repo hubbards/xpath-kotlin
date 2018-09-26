dc88a7d3e728c5613f5dc7a500e8a2a509e663a7
Spencer Hubbard <hubbardspencerm@gmail.com>
2018-09-25 19:51:46 -0700

    Add Dokka Gradle tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
+import org.gradle.jvm.tasks.Jar
+import org.jetbrains.dokka.gradle.DokkaTask
+
 plugins {
   kotlin("jvm") version "1.2.71"
+  id("org.jetbrains.dokka") version "0.9.16"
 }
 
 repositories {
@@ -10,4 +14,16 @@ dependencies {
   implementation(kotlin("stdlib", "1.2.71"))
   testImplementation("junit:junit:4.12")
   testImplementation(kotlin("test", "1.2.71"))
+}
+
+val dokka by tasks.getting(DokkaTask::class) {
+  outputFormat = "html"
+  outputDirectory = "$buildDir/javadoc"
+}
+
+val dokkaJar by tasks.creating(Jar::class) {
+  group = JavaBasePlugin.DOCUMENTATION_GROUP
+  description = "Assembles Kotlin docs with Dokka"
+  classifier = "javadoc"
+  from(dokka)
 }
