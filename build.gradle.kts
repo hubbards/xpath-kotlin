544d9ad892f1a665a1cc960edf3e3fbfb59c6fd3
Spencer Hubbard <hubbardspencerm@gmail.com>
2018-09-27 17:43:54 -0700

    Update build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,15 @@
-import org.gradle.jvm.tasks.Jar
-import org.jetbrains.dokka.gradle.DokkaTask
+//import com.jfrog.bintray.gradle.BintrayExtension
+//import org.gradle.jvm.tasks.Jar
+//import org.jetbrains.dokka.gradle.DokkaTask
+
+group = "com.github.hubbards"
+version = "0.0.1"
 
 plugins {
+  //`maven-publish`
   kotlin("jvm") version "1.2.71"
-  id("org.jetbrains.dokka") version "0.9.16"
+  //id("org.jetbrains.dokka") version "0.9.16"
+  //id("com.jfrog.bintray") version "1.8.4"
 }
 
 repositories {
@@ -16,14 +22,43 @@ dependencies {
   testImplementation(kotlin("test", "1.2.71"))
 }
 
-val dokka by tasks.getting(DokkaTask::class) {
-  outputFormat = "html"
-  outputDirectory = "$buildDir/javadoc"
-}
+//publishing {
+//  publications {
+//    create("default", MavenPublication::class.java) {
+//      from(components["java"])
+//      artifact(dokkaJar)
+//    }
+//    repositories {
+//      maven {
+//        url = uri("$buildDir/repository")
+//      }
+//    }
+//  }
+//}
 
-val dokkaJar by tasks.creating(Jar::class) {
-  group = JavaBasePlugin.DOCUMENTATION_GROUP
-  description = "Assembles Kotlin docs with Dokka"
-  classifier = "javadoc"
-  from(dokka)
-}
+//bintray {
+//  user = property("user") as String
+//  key = property("key") as String
+//  setPublications("default")
+//  pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
+//    repo = "maven"
+//    name = "xpath-kotlin"
+//    vcsUrl = "https://github.com/hubbards/xpath-kotlin.git"
+//    setLicenses("MIT")
+//    version(delegateClosureOf<BintrayExtension.VersionConfig> {
+//      name = "0.0.1"
+//    })
+//  })
+//}
+
+//val dokka by tasks.getting(DokkaTask::class) {
+//  outputFormat = "html"
+//  outputDirectory = "$buildDir/javadoc"
+//}
+
+//val dokkaJar by tasks.creating(Jar::class) {
+//  group = JavaBasePlugin.DOCUMENTATION_GROUP
+//  description = "Assembles a JAR archive for dokka documentation"
+//  classifier = "javadoc"
+//  from(dokka)
+//}
