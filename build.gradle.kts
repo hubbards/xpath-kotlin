61436f2eb6d72801bde8f616d85310543694a1fb
Spencer Hubbard <hubbardspencerm@gmail.com>
2019-06-08 12:10:08 -0700

    Update build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,16 @@
 //import com.jfrog.bintray.gradle.BintrayExtension
-//import org.gradle.jvm.tasks.Jar
-//import org.jetbrains.dokka.gradle.DokkaTask
+import org.gradle.jvm.tasks.Jar
+import org.jetbrains.dokka.gradle.DokkaTask
 
 group = "com.github.hubbards"
 version = "0.0.3"
 
 plugins {
-  //`maven-publish`
-  kotlin("jvm") version "1.3.0"
-  //id("org.jetbrains.dokka") version "0.9.16"
+  `maven-publish`
+
+  kotlin("jvm") version "1.3.31"
+
+  id("org.jetbrains.dokka") version "0.9.18"
   //id("com.jfrog.bintray") version "1.8.4"
 }
 
@@ -17,64 +19,69 @@ repositories {
 }
 
 dependencies {
-  implementation(kotlin(module = "stdlib", version = "1.3.0"))
+  implementation(kotlin(module = "stdlib"))
+
   testImplementation(group = "junit", name = "junit", version = "4.12")
-  testImplementation(kotlin(module = "test", version = "1.3.0"))
+  testImplementation(kotlin(module = "test"))
 }
 
-//tasks {
-//  val dokka by getting(DokkaTask::class) {
-//    outputFormat = "html"
-//    outputDirectory = "$buildDir/javadoc"
-//  }
-//
-//  val dokkaJar by creating(Jar::class) {
-//    group = JavaBasePlugin.DOCUMENTATION_GROUP
-//    description = "Assembles a JAR archive for dokka documentation"
-//    classifier = "javadoc"
-//    from(dokka)
-//  }
-//
-//  val sourceJar by creating(Jar::class) {
-//    dependsOn(JavaPlugin.CLASSES_TASK_NAME)
-//    description = "Assembles a JAR archive for source code"
-//    classifier = "sources"
-//    from(kotlin.sourceSets["main"].kotlin)
-//  }
-//
-//  artifacts {
-//    add("archives", dokkaJar)
-//    add("archives", sourceJar)
-//  }
-//}
+tasks.named<DokkaTask>("dokka") {
+  outputFormat = "html"
+  outputDirectory = "$buildDir/javadoc"
+}
 
-//publishing {
-//  repositories {
-//    maven {
-//      url = uri("$buildDir/repository")
-//    }
-//  }
-//  publications {
-//    create("default", MavenPublication::class.java) {
-//      from(components["java"])
-//      artifact(tasks["dokkaJar"])
-//      artifact(tasks["sourceJar"])
-//    }
-//  }
-//}
+tasks.create<Jar>("dokkaJar") {
+  description = "Assembles a JAR archive for dokka documentation."
+  group = JavaBasePlugin.DOCUMENTATION_GROUP
+
+  archiveClassifier.set("javadoc")
+
+  from(tasks.dokka)
+}
+
+tasks.create<Jar>("sourcesJar") {
+  description = "Assembles a JAR archive for source code."
+  group = BasePlugin.BUILD_GROUP
+
+  archiveClassifier.set("sources")
+
+  from(kotlin.sourceSets["main"].kotlin)
+  dependsOn(JavaPlugin.CLASSES_TASK_NAME)
+}
+
+publishing {
+  publications {
+    create<MavenPublication>("default") {
+      artifact(tasks["dokkaJar"])
+      artifact(tasks["sourcesJar"])
+      from(components["kotlin"])
+    }
+  }
+
+  repositories {
+    maven {
+      url = uri("$buildDir/repository")
+    }
+  }
+}
 
 // when publishing, run ./gradlew build bintrayUpload -Puser=... -Pkey=...
+
 //bintray {
-//  user = property("user") as String
-//  key = property("key") as String
+//  fun findProperty(s: String) = project.findProperty(s) as String?
+//
+//  user = findProperty("user")
+//  key = findProperty("key")
+//
 //  setPublications("default")
+//
 //  pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
 //    repo = "maven"
 //    name = "xpath-kotlin"
 //    vcsUrl = "https://github.com/hubbards/xpath-kotlin.git"
 //    setLicenses("MIT")
 //    version(delegateClosureOf<BintrayExtension.VersionConfig> {
-//      name = "0.0.3"
+//      name = project.version.toString()
 //    })
 //  })
 //}
