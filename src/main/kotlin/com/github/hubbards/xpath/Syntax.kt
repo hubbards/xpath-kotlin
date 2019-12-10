7535802cc0fadf9f91d266113a090e113a914b56
Spencer Hubbard <hubbardspencerm@gmail.com>
2019-12-10 12:58:30 -0800

    Add documentation to Syntax

--- src/main/kotlin/com/github/hubbards/xpath/Syntax.kt
+++ src/main/kotlin/com/github/hubbards/xpath/Syntax.kt
@@ -4,7 +4,13 @@ package com.github.hubbards.xpath
  * Represents a term in a syntactic category of the XPath grammar.
  */
 interface Syntax {
+  /**
+   * Return a representation of this term using unabbreviated syntax.
+   */
   fun unabbreviated(): String
 
+  /**
+   * Return a representation of this term using abbreviated syntax.
+   */
   fun abbreviated(): String
 }
