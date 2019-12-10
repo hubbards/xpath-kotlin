433396a92a5b70ab6f28a085662c2256c1073809
Spencer Hubbard <hubbardspencerm@gmail.com>
2019-12-10 12:59:13 -0800

    Add documentation to XPath.kt

--- src/main/kotlin/com/github/hubbards/xpath/XPath.kt
+++ src/main/kotlin/com/github/hubbards/xpath/XPath.kt
@@ -4,19 +4,44 @@ import org.xml.sax.InputSource
 import javax.xml.namespace.QName
 import javax.xml.xpath.XPath
 
+/**
+ * Compile [expression] for later evaluation.
+ *
+ * @see javax.xml.xpath.XPath.compile
+ */
 fun XPath.compile(expression: Expression) =
     this.compile(expression.toString())
 
+/**
+ * Evaluate [expression] in context of [source].
+ *
+ * @see javax.xml.xpath.XPath.evaluate
+ */
 fun XPath.evaluate(expression: Expression, source: InputSource) =
     this.evaluate(expression.toString(), source)
 
+/**
+ * Evaluate [expression] in context of [source] with [returnType].
+ *
+ * @see javax.xml.xpath.XPath.evaluate
+ */
 fun XPath.evaluate(expression: Expression,
                    source: InputSource,
                    returnType: QName) =
     this.evaluate(expression.toString(), source, returnType)
 
+/**
+ * Evaluate [expression] in context of [item].
+ *
+ * @see javax.xml.xpath.XPath.evaluate
+ */
 fun XPath.evaluate(expression: Expression, item: Any) =
     this.evaluate(expression.toString(), item)
 
+/**
+ * Evaluate [expression] in context of [item] with [returnType].
+ *
+ * @see javax.xml.xpath.XPath.evaluate
+ */
 fun XPath.evaluate(expression: Expression, item: Any, returnType: QName) =
     this.evaluate(expression.toString(), item, returnType)
