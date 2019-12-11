3eb501bd1eeefff58e3a4795010d4e827929cdf4
Spencer Hubbard <hubbardspencerm@gmail.com>
2019-12-10 17:17:33 -0800

    Add number and string documentation

--- src/main/kotlin/com/github/hubbards/xpath/LiteralNumber.kt
+++ src/main/kotlin/com/github/hubbards/xpath/LiteralNumber.kt
@@ -1,5 +1,9 @@
 package com.github.hubbards.xpath
 
+/**
+ * A number literal, see
+ * [specification](https://www.w3.org/TR/1999/REC-xpath-19991116/#numbers).
+ */
 class LiteralNumber(number: Number) : Expression() {
   private val syntax = number.toString()
 
