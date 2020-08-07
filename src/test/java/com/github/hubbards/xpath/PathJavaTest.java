64403a917149b290b036425fe604921049266ad4
Spencer Hubbard <hubbardspencerm@gmail.com>
2020-08-07 08:20:05 -0700

    Format test code

--- src/test/java/com/github/hubbards/xpath/PathJavaTest.java
+++ src/test/java/com/github/hubbards/xpath/PathJavaTest.java
@@ -6,18 +6,16 @@ import org.junit.Test;
 public class PathJavaTest {
     @Test
     public void predicateSyntax() {
-        Expression expression = new Expression.Path.Relative(
-                new Step(
-                        Axis.CHILD,
-                        "para",
-                        new Expression.BinaryExpression(
-                                Operator.EQUAL,
-                                new Expression.Path.Relative(new Step(Axis.ATTRIBUTE, "type")),
-                                new Expression.LiteralString("warning")
-                        ),
-                        new Expression.LiteralNumber(5)
-                )
-        );
+        Expression expression = new Expression.Path.Relative(new Step(
+                Axis.CHILD,
+                "para",
+                new Expression.BinaryExpression(
+                        Operator.EQUAL,
+                        new Expression.Path.Relative(new Step(Axis.ATTRIBUTE, "type")),
+                        new Expression.LiteralString("warning")
+                ),
+                new Expression.LiteralNumber(5)
+        ));
         Assert.assertEquals("child::para[attribute::type = 'warning'][5]", expression.getUnabbreviated());
         Assert.assertEquals("para[@type = 'warning'][5]", expression.getAbbreviated());
     }
