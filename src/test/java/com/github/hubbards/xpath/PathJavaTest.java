e303a22a5321f1d52cf92cb242c937d8ff4f3dd0
Spencer Hubbard <hubbardspencerm@gmail.com>
2020-08-06 17:06:45 -0700

    Add PathJavaTest

---
+++ src/test/java/com/github/hubbards/xpath/PathJavaTest.java
@@ -0,0 +1,24 @@
+package com.github.hubbards.xpath;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PathJavaTest {
+    @Test
+    public void predicateSyntax() {
+        Expression expression = new Expression.Path.Relative(
+                new Step(
+                        Axis.CHILD,
+                        "para",
+                        new Expression.BinaryExpression(
+                                Operator.EQUAL,
+                                new Expression.Path.Relative(new Step(Axis.ATTRIBUTE, "type")),
+                                new Expression.LiteralString("warning")
+                        ),
+                        new Expression.LiteralNumber(5)
+                )
+        );
+        Assert.assertEquals("child::para[attribute::type = 'warning'][5]", expression.getUnabbreviated());
+        Assert.assertEquals("para[@type = 'warning'][5]", expression.getAbbreviated());
+    }
+}
